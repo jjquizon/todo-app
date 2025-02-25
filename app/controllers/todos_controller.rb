412d0bd4ebd4204d287821587ca8240d90397397
jjquizon <jeffrey.quizon@gmail.com>
2025-02-25 01:07:46 -0800

    Add Todos controller, migrate, and add route

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,36 @@
+class TodosController < ApplicationController
+  def index
+    todos = Todo.all
+    render json: todos
+  end
+
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: todo, status: :created
+    else
+      render json: todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      render json: todo, status: :updated
+    else
+      render json: todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    render json: todo, status: :deleted
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
+end
